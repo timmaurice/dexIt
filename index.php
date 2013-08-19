5dd56081817660d6a94e84ba90b886e36a30be76
febLey <mail@febley.com>
2013-08-19 20:40:19 +0200

    Some preparations

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>DexIt</title>
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+  
+</body>
+</html>
